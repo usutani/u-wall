7b3c8427136acc2f89051340da72372a25fd7103
Yasuhiro Usutani <yasuhiro.usutani@gmail.com>
2010-12-23 00:29:12 +0900

    Removed index.html.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 UWall::Application.routes.draw do
   resources :web_clips
+  
+  root :to => "web_clips#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
