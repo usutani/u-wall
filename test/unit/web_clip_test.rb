9f308631c1e90cad61dd9d04b1d5dd711d7f6359
Yasuhiro Usutani <yasuhiro.usutani@gmail.com>
2011-01-01 22:50:55 +0900

    unit testing

--- test/unit/web_clip_test.rb
+++ test/unit/web_clip_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class WebClipTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :web_clips
+  
+  test "webclip attributes must not be empty" do
+    web_clip = WebClip.new
+    assert web_clip.invalid?
+    assert web_clip.errors[:title].any?
+    assert web_clip.errors[:url].any?
+  end
+  
+  test "webclip is not valid without a unique url" do
+    web_clip = WebClip.new(:title => web_clips(:tool_studio).title,
+                           :url   => web_clips(:tool_studio).url, 
+                           :quote => "note", 
+                           :note  => "quote")
+    
+    assert !web_clip.save
+    assert_equal "has already been taken", web_clip.errors[:url].join('; ')
   end
 end
